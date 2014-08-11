3dec274fb0ebf085ce9fa2bd435a3d74fb0fea01
andreas jansson <andreas.s.t.jansson@gmail.com>
2014-08-11 02:57:36 -0400

    clean up
    Change-Id: Iaf16bd895f4e6f1feb167813a63da1bf12c72a95

--- fabfile.py
+++ fabfile.py
@@ -1,6 +1,4 @@
 from headintheclouds.tasks import *
 from headintheclouds import ec2
-from headintheclouds import digitalocean
 from headintheclouds import ensemble
 from headintheclouds import docker
-#ec2.settings['user'] = 'ec2-user'
